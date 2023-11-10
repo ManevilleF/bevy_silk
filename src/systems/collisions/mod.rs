0857c0990c7c18f054f432337aacb9a3e5dc8bce
Joona Aalto <jondolf.dev@gmail.com>
2023-11-10 17:46:46 +0200

    Add `bevy_xpbd` support

---
+++ src/systems/collisions/mod.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "rapier_collisions")]
+pub mod rapier;
+#[cfg(feature = "xpbd_collisions")]
+pub mod xpbd;
---
+++ src/systems/collisions/xpbd.rs
@@ -0,0 +1,105 @@
+#![allow(
+    clippy::needless_pass_by_value,
+    clippy::type_complexity,
+    clippy::option_if_let_else,
+    clippy::suboptimal_flops
+)]
+use crate::components::{cloth::Cloth, collider::ClothCollider};
+use bevy::{log, prelude::*, render::primitives::Aabb};
+use bevy_xpbd_3d::prelude::*;
+
+fn get_collider(aabb: &Aabb, collider: &ClothCollider) -> Collider {
+    let extents = aabb.half_extents * 2.0 + collider.offset;
+    Collider::compound(vec![(
+        Position(aabb.center.into()),
+        Quat::IDENTITY,
+        Collider::cuboid(extents.x, extents.y, extents.z),
+    )])
+}
+
+pub fn handle_collisions(
+    mut cloth_query: Query<(Entity, &mut Cloth, &Aabb, &ClothCollider, &mut Collider)>,
+    collisions: Res<Collisions>,
+    mut colliders_query: Query<
+        (
+            &Collider,
+            &GlobalTransform,
+            Option<&mut LinearVelocity>,
+            Option<&mut AngularVelocity>,
+        ),
+        Without<Cloth>,
+    >,
+    time: Res<Time>,
+) {
+    let delta_time = time.delta_seconds();
+    for (entity, mut cloth, aabb, collider, mut xpbd_collider) in &mut cloth_query {
+        for contact_pair in collisions.collisions_with_entity(entity) {
+            let other_entity = if contact_pair.entity1 == entity {
+                contact_pair.entity2
+            } else {
+                contact_pair.entity1
+            };
+            let Ok((
+                other_collider,
+                other_transform,
+                other_linear_velocity,
+                other_angular_velocity,
+            )) = colliders_query.get_mut(other_entity)
+            else {
+                log::error!("Couldn't find collider on entity {:?}", entity);
+                continue;
+            };
+            let vel = other_linear_velocity.as_ref().map_or(0.0, |velocity| {
+                velocity.length_squared() * delta_time * delta_time * collider.velocity_coefficient
+            });
+            cloth.solve_collisions(|point| {
+                let other_transform = other_transform.compute_transform();
+                let projection = other_collider.shape_scaled().project_point(
+                    &bevy_xpbd_3d::parry::math::Isometry::new(
+                        other_transform.translation.into(),
+                        other_transform.rotation.to_scaled_axis().into(),
+                    ),
+                    &(*point).into(),
+                    false,
+                );
+                let projected_point = Vec3::from(projection.point);
+                let normal: Vec3 = (projected_point - *point)
+                    .try_normalize()
+                    .unwrap_or(Vec3::Y);
+
+                if projection.is_inside {
+                    Some(projected_point + (normal * collider.offset) + (normal * vel))
+                } else if point.distance_squared(projected_point)
+                    < collider.offset * collider.offset
+                {
+                    Some(projected_point - (normal * collider.offset))
+                } else {
+                    None
+                }
+            });
+            if let Some(((ref mut lin_vel, ref mut ang_vel), dampen_coef)) = other_linear_velocity
+                .zip(other_angular_velocity)
+                .zip(collider.dampen_others)
+            {
+                let damp = 1.0 - dampen_coef;
+                lin_vel.0 *= damp;
+                ang_vel.0 *= damp;
+            }
+        }
+        *xpbd_collider = get_collider(aabb, collider);
+    }
+}
+
+pub fn init_cloth_collider(
+    mut commands: Commands,
+    cloth_query: Query<(Entity, &Aabb, &ClothCollider), (With<Cloth>, Without<Collider>)>,
+) {
+    for (entity, aabb, collider) in cloth_query.iter() {
+        log::debug!("Initializing Cloth collisions for {:?}", entity);
+        commands.entity(entity).insert((
+            RigidBody::Kinematic,
+            Sensor,
+            get_collider(aabb, collider),
+        ));
+    }
+}
